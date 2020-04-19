4de51c6a1dea570bac44617cab5ed074d3504ae0
Tony Park <tonypark0403@gmail.com>
2020-04-19 15:42:29 -0400

    applied schema in a better way

--- src/server.js
+++ src/server.js
@@ -2,26 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import logger from "morgan";
-import graphqlYoga from "graphql-yoga";
-const { GraphQLServer } = graphqlYoga;
+import schema from "./schema";
+
+import { GraphQLServer } from "graphql-yoga";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        greeting: String!
-    }
-`;
-
-const resolvers = {
-  Query: {
-    greeting: () => "Hi",
-  },
-};
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`✅Server running on http://localhost:${PORT}`)
 );
