d031c3ee134653825e9ae7dc7f5453ce0772312a
Tony Park <tonypark0403@gmail.com>
2020-04-20 19:48:58 -0400

    refactoring fullPost with fragments

--- src/fragments.js
+++ src/fragments.js
@@ -1,24 +1,34 @@
 export const USER_FRAGMENT = `
-    fragment UserParts on User {
-        id,
-        username,
-        email,
-        firstName,
-        lastName,
-        bio
-        posts {
-            id
-            caption
-        }
-    }
+    id
+    username
 `;
 
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment {
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
         id
-        text
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
         user {
-            username
+            ${USER_FRAGMENT}
         }
     }
 `;
