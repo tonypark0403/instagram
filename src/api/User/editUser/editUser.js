fb8611c7c074d553bf2e3f81f2bca51684878ff0
Tony Park <tonypark0403@gmail.com>
2020-04-20 10:13:48 -0400

    added edit user

---
+++ src/api/User/editUser/editUser.js
@@ -0,0 +1,24 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    editUser: async (_, args, { request }) => {
+      isAuthenticated(request);
+      const { username, email, firstName, lastName, bio } = args;
+      const { user } = request;
+      return prisma.updateUser({
+        where: {
+          id: user.id,
+        },
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          bio,
+        },
+      });
+    },
+  },
+};
