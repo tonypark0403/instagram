ff6189996121d90ef8b6806d119775017ff44e47
Tony Park <tonypark0403@gmail.com>
2020-04-21 18:40:23 -0400

    added hash router

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "../styles/GlobalStyles";
-import Theme from "../styles/Theme";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import Router from "./Router";
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
+    <>
+      <GlobalStyles />
+      <Router isLoggedIn={false} />
+    </>
   </ThemeProvider>
 );
-
-export default App;
