21ebe42151d3db7432e5f0118757e489b6855b6c
Tony Park <tonypark0403@gmail.com>
2020-04-20 20:13:09 -0400

    added enum to divide edit and delete

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -1,25 +1,34 @@
 import { prisma } from "../../../../generated/prisma-client";
 
+const DELETE = "DELETE";
+const EDIT = "EDIT";
+
 export default {
   Mutation: {
     editPost: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      const { id, caption, location } = args;
+      const { id, caption, location, action } = args;
       const { user } = request;
       const post = await prisma.$exists.post({
         id,
         user: { id: user.id },
       });
       if (post) {
-        return prisma.updatePost({
-          data: {
-            caption,
-            location,
-          },
-          where: {
+        if (action === EDIT) {
+          return prisma.updatePost({
+            data: {
+              caption,
+              location,
+            },
+            where: {
+              id,
+            },
+          });
+        } else if (action === DELETE) {
+          return prisma.deletePost({
             id,
-          },
-        });
+          });
+        }
       } else {
         throw Error("You can't do that");
       }
