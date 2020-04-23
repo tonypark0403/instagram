89c348acc2da3a5167ef29a3b2571677ee428958
Tony Park <tonypark0403@gmail.com>
2020-04-22 20:44:36 -0400

    modified error message in backend

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,6 +3,10 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("This username / email is already taken!");
+      }
       await prisma.createUser({
         username,
         email,
