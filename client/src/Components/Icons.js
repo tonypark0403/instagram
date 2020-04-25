38d0de2798faab7eee88a264ca370ff57ae29fa4
Tony Park <tonypark0403@gmail.com>
2020-04-25 15:45:36 -0400

    finished search part for post

--- client/src/Components/Icons.js
+++ client/src/Components/Icons.js
@@ -56,6 +56,17 @@ export const Comment = ({ size = 20 }) => (
   </svg>
 );
 
+export const CommentFull = ({ size = 20 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 1c-6.627 0-12 4.364-12 9.749 0 3.131 1.817 5.917 4.64 7.7.868 2.167-1.083 4.008-3.142 4.503 2.271.195 6.311-.121 9.374-2.498 7.095.538 13.128-3.997 13.128-9.705 0-5.385-5.373-9.749-12-9.749z" />
+  </svg>
+);
+
 export const Email = ({ size = 20 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
